bb9dbd06bac15f41a99e1ebbe10945818845228b
somlosocram-star <somlosocram@gmail.com>
2026-06-01 14:38:36 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    Precarga la cáscara (juego jugable offline en local/IA).
    La música y las fuentes se cachean la primera vez que se usan.
    El online (Firebase) siempre va a la red; no se cachea. */
-const VERSION = 'caza-v21';
+const VERSION = 'caza-v22';
 const SHELL = [
   './',
   './index.html',
